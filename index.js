379f1074a27a5e68e47ac55f8a1f4d58b650ee7c
Ankur <mern.ankur@gmail.com>
2022-09-08 08:52:18 +0530

    create public folder

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 const port = process.env.PORT || 80
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/public/index.html");
     // res.send("hello node")
 })
 app.get("/api/:input", (req, res) => {
